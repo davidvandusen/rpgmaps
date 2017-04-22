f842f9beda62aeb6ed32cf78c5d89aa06020df7c
David VanDusen <github@davidvandusen.com>
2017-04-21 22:44:15 -0700

    Fade from edge of river

--- client/src/terrains/CloseUpRiver.js
+++ client/src/terrains/CloseUpRiver.js
@@ -1,6 +1,4 @@
 const BaseTerrain = require('./BaseTerrain');
-const {rgbaToCss} = require('../common/color');
-const {offsetMask, outlineMask, smoothPolygon} = require('../common/geometry');
 
 class CloseUpRiver extends BaseTerrain {
   base() {
@@ -9,8 +7,15 @@ class CloseUpRiver extends BaseTerrain {
       this.clipShape(this.smoothOutlineShape);
       this.ctx.fillStyle = 'rgb(132,184,204)';
       this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-      this.ctx.strokeStyle = 'rgba(162,214,234, 0.2)';
+      this.ctx.strokeStyle = 'rgba(234,214,162, 0.2)';
       this.ctx.lineWidth = this.scaleFactorX * 2.5;
+      this.smoothOutlineShape.forEach(path => {
+        this.ctx.beginPath();
+        this.drawPath(path);
+        this.ctx.closePath();
+        this.ctx.stroke();
+      });
+      this.ctx.lineWidth = this.scaleFactorX * 1.5;
       this.smoothOutlineShape.forEach(path => {
         this.ctx.beginPath();
         this.drawPath(path);
